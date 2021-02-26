11d170ff0a2d821720d79550c1451b13a048daf4
Jefferson <jefergiral@gmail.com>
2021-02-26 14:54:45 -0500

    Se añadió la funcionalidad modo nocturno

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// SECTION FOOTER
+// DARK MODE
+
+const linkDarkMode = document.querySelector('#dark-mode');
+
+linkDarkMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  linkDarkMode.classList.toggle('active');
+})
