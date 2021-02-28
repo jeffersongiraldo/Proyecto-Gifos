e81ecd924e46c9b8c4c7d9b93867318ee696620c
Jefferson <jefergiral@gmail.com>
2021-02-28 10:41:43 -0500

    Se agregaron comentarios en el codigo de estilos y en el JavaScript

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ linkDarkMode.addEventListener('click', () => {
 })
 
 
-
+// Variables para cambiar el contenido central de la pagina entre los links favoritos y mis gifos
 const linkFavoritos = document.querySelector('#link-favoritos');
 const sectionMainSearch = document.querySelector('#section-main-search');
 const sectionFavoritos = document.querySelector('#section-favoritos');
@@ -16,18 +16,18 @@ const linkMisGifos = document.querySelector('#link-mis-gifos');
 
 // Se activa seccion mis favoritos
 linkFavoritos.addEventListener('click', () => {
-  sectionMainSearch.classList.add('desactiveSection');
-  sectionFavoritos.classList.add('activeSection');
-  sectionMisGifos.classList.remove('activeSection');
-  linkFavoritos.classList.add('activeLink');
-  linkMisGifos.classList.remove('activeLink');
+  sectionMainSearch.classList.add('desactiveSection'); // Se agrega la clase desactiveSection
+  sectionFavoritos.classList.add('activeSection'); // Se agrega la clase activeSection para mostrar el contenido en la pagina
+  sectionMisGifos.classList.remove('activeSection'); // Se remueve la clase activeSection
+  linkFavoritos.classList.add('activeLink'); // Se agrega la clase activeLink 
+  linkMisGifos.classList.remove('activeLink'); // Se remueve la clase activeLink
 })
 
-// Se activa seccion mis favoritos
+// Se activa seccion mis gifos
 linkMisGifos.addEventListener('click', () => {
-  sectionMainSearch.classList.add('desactiveSection');
-  sectionFavoritos.classList.remove('activeSection');
-  sectionMisGifos.classList.add('activeSection');
+  sectionMainSearch.classList.add('desactiveSection'); 
+  sectionFavoritos.classList.remove('activeSection'); 
+  sectionMisGifos.classList.add('activeSection'); 
   linkFavoritos.classList.remove('activeLink');
   linkMisGifos.classList.add('activeLink');
 })
