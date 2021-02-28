bbc1c4a71de10a02844dc6647c29b4cf72cea446
Jefferson <jefergiral@gmail.com>
2021-02-28 01:33:59 -0500

    Se añade la funcionalidad para ingresar a los links favoritos y mis gifos sin contenido

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 // DARK MODE
 
 const linkDarkMode = document.querySelector('#dark-mode');
-
 linkDarkMode.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   linkDarkMode.classList.toggle('active');
+})
+
+
+
+const linkFavoritos = document.querySelector('#link-favoritos');
+const sectionMainSearch = document.querySelector('#section-main-search');
+const sectionFavoritos = document.querySelector('#section-favoritos');
+const sectionMisGifos = document.querySelector('#section-mis-gifos')
+const linkMisGifos = document.querySelector('#link-mis-gifos');
+
+// Se activa seccion mis favoritos
+linkFavoritos.addEventListener('click', () => {
+  sectionMainSearch.classList.add('desactiveSection');
+  sectionFavoritos.classList.add('activeSection');
+  sectionMisGifos.classList.remove('activeSection');
+  linkFavoritos.classList.add('activeLink');
+  linkMisGifos.classList.remove('activeLink');
+})
+
+// Se activa seccion mis favoritos
+linkMisGifos.addEventListener('click', () => {
+  sectionMainSearch.classList.add('desactiveSection');
+  sectionFavoritos.classList.remove('activeSection');
+  sectionMisGifos.classList.add('activeSection');
+  linkFavoritos.classList.remove('activeLink');
+  linkMisGifos.classList.add('activeLink');
 })
